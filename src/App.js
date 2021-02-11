b3645ed026409d79974a8b14b99cc52ffdca491b
fjrois <fjroissiso@gmail.com>
2021-02-10 22:24:05 -0800

    Removed initial React elements from App, and included Blog and new props for FirstComponent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Blog from './components/Blog';
 import FirstComponent from './components/FirstComponent';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <FirstComponent />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isBlogDisplayed: false };
+    this.showHideBlog = this.showHideBlog.bind(this);
+  }
+
+  showHideBlog() {
+    this.setState((state) => {
+      return { isBlogDisplayed: !state.isBlogDisplayed };
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FirstComponent
+          isBlogDisplayed={this.state.isBlogDisplayed}
+          showHideBlog={this.showHideBlog}
+        />
+        {this.state.isBlogDisplayed ? <Blog /> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
