3ab580e26527e35311d535d082ba9f89dcdeaa1b
fjrois <fjroissiso@gmail.com>
2021-02-10 22:22:54 -0800

    Added main site, alert me and blog buttons to FirstComponent

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,14 +1,44 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class FirstComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  showAlert = () => {
+    window.alert('Ok, this is an alert.\nNow what?');
+  };
+
+  redirectToMainSite = () => {
+    window.open('https://www.fjrois.com');
+  };
   render() {
+    const { isBlogDisplayed } = this.props;
     return (
       <>
         <p>This is just the beginning.</p>
         <p>Por algo hay que empezar!!</p>
+
+        <button onClick={this.redirectToMainSite}>Main site</button>
+        <button onClick={this.showAlert}>Alert me</button>
+        <button
+          onClick={this.props.showHideBlog}
+          style={{
+            backgroundColor: isBlogDisplayed ? 'black' : 'white',
+            color: isBlogDisplayed ? 'white' : 'black',
+          }}
+        >
+          Blog
+        </button>
       </>
     );
   }
 }
+
+FirstComponent.propTypes = {
+  isBlogDisplayed: PropTypes.bool.isRequired,
+  showHideBlog: PropTypes.func.isRequired,
+};
 
 export default FirstComponent;
