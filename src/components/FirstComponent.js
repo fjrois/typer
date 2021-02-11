eac43e9d703b5d809107d9376edbde145b4272e6
fjrois <fjroissiso@gmail.com>
2021-02-10 21:14:06 -0800

    Replace initial React logo by initial custom component

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FirstComponent extends React.Component {
+  render() {
+    return (
+      <>
+        <p>This is just the beginning.</p>
+        <p>Por algo hay que empezar!!</p>
+      </>
+    );
+  }
+}
+
+export default FirstComponent;
