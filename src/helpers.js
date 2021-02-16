4cecb97861b5ad70f701d1703a26bf5b837e301b
fjrois <fjroissiso@gmail.com>
2021-02-15 17:27:32 -0800

    Created helper for decimal numbers formatting

--- src/helpers.js
+++ src/helpers.js
@@ -2,3 +2,12 @@ export function capitalizeFirstLetter(str) {
   const result = `${str[0].toUpperCase()}${str.substring(1)}`;
   return result;
 }
+
+// Leave a certain number of decimal digits, without any rounding
+export function formatDecimalNumber(num, numberOfDecimals) {
+  // Leave 3 decimals by default
+  if (!numberOfDecimals) numberOfDecimals = 3;
+  const aux = Math.pow(10, numberOfDecimals);
+  const formattedNum = Math.floor(num * aux) / aux;
+  return formattedNum;
+}
