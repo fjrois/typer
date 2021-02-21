3e952db6d543e536447390fd81cd9d6c6727cb29
fjrois <fjroissiso@gmail.com>
2021-02-21 12:31:55 -0800

    Added Footer with logo credits

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+export default function Footer(props) {
+  return (
+    <>
+      <div className="footer">
+        <div className="footer-text">
+          © 2021 fjrois
+          {/* <div className="footer-icon-credits"> */} | Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/authors/nikita-golubev"
+            title="Nikita Golubev"
+          >
+            Nikita Golubev
+          </a>
+          {/* </div> */}
+        </div>
+      </div>
+    </>
+  );
+}
