935e85baba82c474368ee5f49163f043ba6d8bc0
fjrois <fjroissiso@gmail.com>
2021-02-21 02:03:38 -0800

    Created Header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header(props) {
+  // return <h2>&#62;Typer&#60;</h2>;
+  // return (
+  //   <h2>
+  //     <code>&#62;Typer&#60;</code>
+  //   </h2>
+  // );
+  return <h1>Typer_</h1>;
+}
